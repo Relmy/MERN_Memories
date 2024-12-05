25f715f9c014521ed797fd9132ff047b3597c362
CJ <rosario.cj.elmy@gmail.com>
2024-12-05 18:48:08 -0500

    Use .env for CONNECTION_URL - Change to new URI

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use("/posts", postRoutes); 
 
-const CONNECTION_URL = "mongodb+srv://admin:admin123@cluster0.pxzqryp.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
